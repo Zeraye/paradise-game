e52c0ae11e5992c9442140131d894f7bcf5c9d7b
Jakub Rudnik <jakubrudnik02@gmail.com>
2020-07-30 22:34:32 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,50 @@
+import pygame
+import os
+
+pygame.init()
+screenObject = pygame.display.Info()
+# basic lengths
+top_border = round((200 * screenObject.current_h) / 1080)
+side_border = round((575 * screenObject.current_w) / 1920)
+side_length = round((70 * screenObject.current_w) / 1920)
+gap = round((64 * screenObject.current_w) / 1920)
+
+
+def scale(length):
+    return round((length * screenObject.current_w) / 1920)
+
+
+# importing images
+pygame.display.set_mode((screenObject.current_w, screenObject.current_h), pygame.FULLSCREEN)
+square_img = pygame.image.load(os.path.join(os.getcwd(), './images/Square.png')).convert_alpha()
+square_img = pygame.transform.scale(square_img, (side_length, side_length))
+corss_img = pygame.image.load(os.path.join(os.getcwd(), './images/Cross.png')).convert_alpha()
+corss_img = pygame.transform.scale(corss_img, (side_length, side_length))
+bigcross_img = pygame.image.load(os.path.join(os.getcwd(), './images/BigCross.png')).convert_alpha()
+bigcross_img = pygame.transform.scale(bigcross_img, (side_length, side_length))
+diagonal_img = pygame.image.load(os.path.join(os.getcwd(), './images/Diagonal.png')).convert_alpha()
+diagonal_img = pygame.transform.scale(diagonal_img, (side_length, side_length))
+
+# colors
+BLUE_DARK = (0, 132, 255)
+BLUE_LIGT = (68, 190, 199)
+YELLOW = (255, 195, 0)
+ORANGE = (255, 165, 0)
+RED = (250, 60, 76)
+VIOLET = (71, 39, 71)
+WHITE = (255, 255, 255)
+GREEN = (127, 255, 0)
+
+# gameplay settings
+obstacles_list = []
+obstacles_homing_list = []
+delay = 1.5
+death = False
+death_time = start_game_time = 0
+homing = False
+last_homing_pos = [0, 10]
+
+# fonts
+font_size_100 = round((100 * screenObject.current_w) / 1920)
+font_size_80 = round((80 * screenObject.current_w) / 1920)
+font_size_60 = round((60 * screenObject.current_w) / 1920)
