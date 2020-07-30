2cd6159a7fabc055c5fbd697e0625c7df9c0273c
Jakub Rudnik <jakubrudnik02@gmail.com>
2020-07-30 22:34:28 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -2,22 +2,21 @@ from board import board
 import random
 
 
-def update_pos(row, col, type):
-    board[row][col].append(type)
+def update_pos(row, col, obstacle_type):
+    board[row][col].append(obstacle_type)
 
 
-def reset_pos(row, col, type):
-    for _ in board[row][col]:
-        if _ == type:
-            board[row][col].remove(_)
+def reset_pos(row, col, obstacle_type):
+    if contain(board[row][col], obstacle_type):
+        board[row][col].remove(obstacle_type)
 
 
-def reset_type(type):
+def reset_type(obstacle_type):
     i = j = 0
     while i < 11:
         while j < 11:
-            if contain(board[i][j], type):
-                reset_pos(i, j, type)
+            if contain(board[i][j], obstacle_type):
+                reset_pos(i, j, obstacle_type)
             j += 1
         j = 0
         i += 1
@@ -34,20 +33,20 @@ def reset_board():
         i += 1
 
 
-def contain(array, type):
+def contain(array, obstacle_type):
     for _ in array:
-        if _ == type:
+        if _ == obstacle_type:
             return True
     return False
 
 
-def search_by_type(type):
+def search_by_type(obstacle_type):
     i = 0
     j = 0
     arr = []
     while i < 11:
         while j < 11:
-            if board[j][j] == int(type):
+            if board[j][j] == int(obstacle_type):
                 arr.append([i, j])
             j += 1
         j = 0
@@ -85,3 +84,11 @@ def random_array():
             array2.append([x, y])
             i += 1
     return array1, array2
+
+
+def print_board():
+    print('BOARD')
+    i = 0
+    while i < len(board):
+        print(board[i])
+        i += 1
