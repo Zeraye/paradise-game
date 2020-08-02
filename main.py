7f117dec88a2c1fc994875e03dc59198bfe05266
Jakub Rudnik <jakubrudnik02@gmail.com>
2020-08-02 15:27:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from menus.main_menu import main_menu_func
-from menus.main_menu import WIN
+import pygame
+import menus.starting_screen
+
+pygame.init()
+
+pygame.mouse.set_visible(False)
+
+screenObject = pygame.display.Info()
+WIDTH = screenObject.current_w
+HEIGHT = screenObject.current_h
+WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+pygame.display.set_caption('Paradise')
 
 if __name__ == '__main__':
-    main_menu_func(WIN)
+    menus.starting_screen.starting_menu_func(WIN)
